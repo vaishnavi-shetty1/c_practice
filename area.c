55282c5617dd6bfc481023cbc5d9c1a35625e019
vaishnavi-shetty1 <shettyvaishnavi871@gmail.com>
2024-09-16 16:41:43 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+float squarearea(float side);
+float circlearea(float rad);
+float rectarea(float a,float b);
+
+int main(){
+    float side,a,b,rad;
+
+    printf("enter side of square:");
+    scanf("%f",&side);
+    printf("area of square is :%f\n",squarearea(side));
+
+    printf("enter radius of circle:");
+    scanf("%f",&rad);
+    printf("area is:%f\n",circlearea(rad));
+
+    printf("enter sides of rectangle:");
+    scanf("%f%f",&a,&b);
+    printf("area of rectangle is:%f\n",rectarea(a,b));
+
+    return 0;
+}
+
+float squarearea(float side){
+    return side + side;
+}
+float circlearea(float rad){
+    return 3.14*rad*rad;
+}
+float rectarea(float a,float b){
+    return a*b;
+}
