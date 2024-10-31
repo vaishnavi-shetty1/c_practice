24fe731a2a33dec9df283f7af7a69a0a58cc9add
vaishnavi-shetty1 <shettyvaishnavi871@gmail.com>
2024-10-31 15:38:31 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,19 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int fact(int n);
 
-int main(){
-    int n;
-    printf("Enter a number to find factorial:");
-    scanf("%d",&n);
-    printf("factorial is %d",fact(n));
-    return 0;
+int main() 
+{
+int n;
+printf("Enter a non-negative integer to find its factorial: ");
+scanf("%d", &n);
+if (n < 0) 
+{
+printf("Factorial is not defined for negative numbers.\n");
 }
-int fact(int n){
-    if(n==0){
-        return 1;
-    }
-    int factnm1 = fact(n-1);
-    int factn= factnm1*n;
-    return factn;
+else 
+{
+printf("Factorial of %d is %d\n", n, fact(n));
+}
+return 0;
+}
+
+int fact(int n) 
+{
+if (n == 0) 
+{
+return 1;  
+}
+return n * fact(n - 1);  
 }
